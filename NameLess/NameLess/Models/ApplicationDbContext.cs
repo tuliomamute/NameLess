912ded45a8353ce10bc97a185ed908a1168f1fbd
Túlio Rezende <tulio.mamute@hotmail.com>
2016-08-08 23:54:44 -0300

    criação de  contexto em classe separada

---
+++ NameLess/NameLess/Models/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NameLess.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        public System.Data.Entity.DbSet<NameLess.Models.Tags> Tags { get; set; }
+        public System.Data.Entity.DbSet<NameLess.Models.CamposPesquisa> CamposPesquisa { get; set; }
+        public System.Data.Entity.DbSet<NameLess.Models.Pesquisas> Pesquisas { get; set; }
+        public System.Data.Entity.DbSet<NameLess.Models.Cliente> Cliente { get; set; }
+
+    }
+}
--- NameLess/NameLess/Models/IdentityModels.cs
+++ NameLess/NameLess/Models/IdentityModels.cs
@@ -18,22 +18,5 @@ namespace NameLess.Models
         }
     }
 
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
-        public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
-        {
-        }
 
-        public static ApplicationDbContext Create()
-        {
-            return new ApplicationDbContext();
-        }
-
-        public System.Data.Entity.DbSet<NameLess.Models.Tags> Tags { get; set; }
-        public System.Data.Entity.DbSet<NameLess.Models.CamposPesquisa> CamposPesquisa { get; set; }
-        public System.Data.Entity.DbSet<NameLess.Models.Pesquisas> Pesquisas { get; set; }
-        public System.Data.Entity.DbSet<NameLess.Models.Cliente> Cliente { get; set; }
-
-    }
 }
