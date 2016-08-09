bde5b0bfa04abaaad388eb01d600635b803043b0
Túlio Rezende <tulio.mamute@hotmail.com>
2016-08-09 00:03:28 -0300

    Criação de views e controllers

---
+++ NameLess/NameLess/Controllers/CamposPesquisasController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using NameLess.Models;
+
+namespace NameLess.Controllers
+{
+    public class CamposPesquisasController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: CamposPesquisas
+        public ActionResult Index()
+        {
+            var camposPesquisa = db.CamposPesquisa.Include(c => c.Tags);
+            return View(camposPesquisa.ToList());
+        }
+
+        // GET: CamposPesquisas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CamposPesquisa camposPesquisa = db.CamposPesquisa.Find(id);
+            if (camposPesquisa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(camposPesquisa);
+        }
+
+        // GET: CamposPesquisas/Create
+        public ActionResult Create()
+        {
+            ViewBag.TagId = new SelectList(db.Tags, "TagId", "Tag");
+            return View();
+        }
+
+        // POST: CamposPesquisas/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CamposPesquisaId,TagId,IdCampo,TipoCampo")] CamposPesquisa camposPesquisa)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CamposPesquisa.Add(camposPesquisa);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.TagId = new SelectList(db.Tags, "TagId", "Tag", camposPesquisa.TagId);
+            return View(camposPesquisa);
+        }
+
+        // GET: CamposPesquisas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CamposPesquisa camposPesquisa = db.CamposPesquisa.Find(id);
+            if (camposPesquisa == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.TagId = new SelectList(db.Tags, "TagId", "Tag", camposPesquisa.TagId);
+            return View(camposPesquisa);
+        }
+
+        // POST: CamposPesquisas/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CamposPesquisaId,TagId,IdCampo,TipoCampo")] CamposPesquisa camposPesquisa)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(camposPesquisa).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.TagId = new SelectList(db.Tags, "TagId", "Tag", camposPesquisa.TagId);
+            return View(camposPesquisa);
+        }
+
+        // GET: CamposPesquisas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CamposPesquisa camposPesquisa = db.CamposPesquisa.Find(id);
+            if (camposPesquisa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(camposPesquisa);
+        }
+
+        // POST: CamposPesquisas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            CamposPesquisa camposPesquisa = db.CamposPesquisa.Find(id);
+            db.CamposPesquisa.Remove(camposPesquisa);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ NameLess/NameLess/Controllers/TagsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using NameLess.Models;
+
+namespace NameLess.Controllers
+{
+    public class TagsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Tags
+        public ActionResult Index()
+        {
+            return View(db.Tags.ToList());
+        }
+
+        // GET: Tags/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tags tags = db.Tags.Find(id);
+            if (tags == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tags);
+        }
+
+        // GET: Tags/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Tags/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "TagId,Tag,DescricaoTag,ObservacaoTag")] Tags tags)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tags.Add(tags);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tags);
+        }
+
+        // GET: Tags/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tags tags = db.Tags.Find(id);
+            if (tags == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tags);
+        }
+
+        // POST: Tags/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "TagId,Tag,DescricaoTag,ObservacaoTag")] Tags tags)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tags).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tags);
+        }
+
+        // GET: Tags/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tags tags = db.Tags.Find(id);
+            if (tags == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tags);
+        }
+
+        // POST: Tags/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Tags tags = db.Tags.Find(id);
+            db.Tags.Remove(tags);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
