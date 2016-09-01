eddf3e38447ef63b992af929944652041791793f
Túlio Rezende <tulio.mamute@hotmail.com>
2016-08-31 21:56:43 -0300

    colocação de css e alteração de bundle.config

--- NameLess/NameLess/App_Start/BundleConfig.cs
+++ NameLess/NameLess/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace NameLess
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/stylish-portfolio.css"));
         }
     }
 }
