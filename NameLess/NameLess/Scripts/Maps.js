cd46b81621eb6e0bef48853bc86668539e174b94
Túlio Rezende <tulio.mamute@hotmail.com>
2016-11-01 13:29:39 -0200

    .

--- NameLess/NameLess/Scripts/Maps.js
+++ NameLess/NameLess/Scripts/Maps.js
@@ -1,4 +1,5 @@
 ﻿var map;
+var map2;
 
 function CarregarPontos(url) {
     var DataInicial = $("#DataInicialPontos").val();
@@ -66,3 +67,14 @@ function CarregarPontos(url) {
     }
 }
 
+function MapaCalor(url) {
+   map2  = L.map('map2').setView([37.8, -96], 4);
+
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
+        maxZoom: 18,
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors,' +
+			'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+			'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        id: 'mapbox.light'
+    }).addTo(map2);
+}
