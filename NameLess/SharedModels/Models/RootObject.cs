b8793dd5a7a06f493dec21542ebdefbce586fb5e
Túlio Rezende <tulio.mamute@hotmail.com>
2016-11-06 15:02:47 -0200

    .

--- NameLess/SharedModels/Models/RootObject.cs
+++ NameLess/SharedModels/Models/RootObject.cs
@@ -1,17 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SharedModels
+namespace SharedModels.Models
 {
+    [NotMapped]
     public class Rootobject
     {
         public Result[] results { get; set; }
         public string status { get; set; }
     }
-
+    [NotMapped]
     public class Result
     {
         public Address_Components[] address_components { get; set; }
@@ -21,6 +23,7 @@ namespace SharedModels
         public string[] types { get; set; }
     }
 
+    [NotMapped]
     public class Geometry
     {
         public Location location { get; set; }
@@ -28,30 +31,35 @@ namespace SharedModels
         public Viewport viewport { get; set; }
     }
 
+    [NotMapped]
     public class Location
     {
         public float lat { get; set; }
         public float lng { get; set; }
     }
 
+    [NotMapped]
     public class Viewport
     {
         public Northeast northeast { get; set; }
         public Southwest southwest { get; set; }
     }
 
+    [NotMapped]
     public class Northeast
     {
         public float lat { get; set; }
         public float lng { get; set; }
     }
 
+    [NotMapped]
     public class Southwest
     {
         public float lat { get; set; }
         public float lng { get; set; }
     }
 
+    [NotMapped]
     public class Address_Components
     {
         public string long_name { get; set; }
